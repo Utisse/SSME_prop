9115575d96c58f75af3979821dd6a9463ec1c78f
Utisse <lorenzopasqui@gmail.com>
2024-04-23 17:51:27 +0200

    Snellita e aggiunta modalità di check

--- TurboPumpOperation.m
+++ TurboPumpOperation.m
@@ -1,21 +1,20 @@
-function [P_in, T_in, P_out, T_out, mdot_out] = TurboPumpOperation(mdot, eff, PR, P_inlet, T_inlet, gamma)
-    % Dati di design
+function [P_out, T_out] = TurboPumpOperation( eta_pump, beta_pump, P_in, T_in, gamma,t_out)
     
-    % Efficienza delle turbopompe
-    eta_pump = eff;
+    if(nargin >= 5)
+        % Calcolo della pressione in uscita
+        P_out = P_in * beta_pump; % Pressione in uscita [Pa]
     
-    % Rapporto di compressione delle turbopompe
-    PR_pump = PR;
-    
-    % Condizioni in ingresso
-    P_in = P_inlet; % Pressione in ingresso [Pa]
-    T_in = T_inlet; % Temperatura in ingresso [K]
-    
-    % Calcolo della pressione in uscita
-    P_out = P_in * PR_pump; % Pressione in uscita [Pa]
-    
-    % Calcolo della temperatura in uscita considerando l'efficienza
-    T_out_id= T_in*PR_pump^((gamma-1)/(gamma)); % Temperatura in uscita ideale [K]
-    T_out = (T_out_id - T_in) / eta_pump ; % considero l'efficienza della turbopompa
-    mdot_out = mdot ; % portata in uscita dalla turbopompa
+        % Calcolo della temperatura in uscita considerando l'efficienza
+        T_out_id= T_in*beta_pump^((gamma-1)/(gamma));% Temperatura in uscita ideale [K]
+        T_out = (T_out_id - T_in) / eta_pump ; % considero l'efficienza della turbopompa
+    end
+    if(nargin == 6)
+        errore_perc = abs(t_out - T_out)/t_out;
+        if(errore_perc < .05)
+            disp('Il valore della temperatura torna');
+        else 
+            eta_vera = (T_out_id - T_in)/t_out;  
+            stampa = ['ERRORE: eta vera = ',num2str(eta_vera)];
+            disp(stampa);
+        end
 end
