11b5e4180bb2579044f4009baceafddfc4f09540
lucapal11 <167205004+lucapal11@users.noreply.github.com>
2024-05-07 19:50:56 +0200

    Update stato.m

--- stato.m
+++ stato.m
@@ -1,20 +1,29 @@
 classdef stato
-    %STATO Classe per raggruppare temp, press e portata
+    % classe per raggruppare tutte le caratteristiche del fluido che stiamo valutando
     %   
     
     properties
         T;
         P;
         Q;
+        p_vap
+        rho ;
+        cv ;
+        cp ;
     end
     
     methods
-        function obj = stato(temperatura,pressione,portata)
+        function obj = stato(temperatura,pressione,portata, pressione_di_vapore, densita, cv, cp)
             %STATO Construct an instance of this class
-            %   Detailed explanation goes here
+            % definizione delle caratteristiche del fluido
             obj.T = temperatura;
             obj.P = pressione;
             obj.Q = portata;
+            obj.p_vap = pressione_di_vapore ;
+            obj.rho = densita;
+            obj.cv = cv ;
+            obj.cp = cp ;
+          
         end
     end
 end
