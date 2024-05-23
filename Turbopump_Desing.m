de423c4a84d57a83b52c1efe8ba4a6a63c64449c
Utisse <lorenzopasqui@gmail.com>
2024-05-23 18:47:45 +0200

    sincronizzati nomi variabili con humble e aggiunto check per quale Vr usare come da pp. 254

--- Turbopump_Desing.m
+++ Turbopump_Desing.m
@@ -7,33 +7,43 @@ Dp = P_out - P_in;% salto di pressione richiesto a cavallo della pompa [Pa]
 if type == 0 
     Dp_ps = 16e6 ; % [Pa] -> masssimo Dp (teorico) per singolo stadio per LH2 da libro "space prop. analysis and design " (Humble)
     disp('FUEL LH2')
-    Vs = 2.0; % stage specific speed per LH2
+    Ns = 2.0; % stage specific speed per LH2
     u_ss = 130 ;% suction specific speed per LH2
     psi = 0.60 ;% pump head coefficient for LH2
 end
 if  type == 1 
     Dp_ps = 47e6 ; % [Pa] -> masssimo Dp (teorico) per singolo stadio per altri fluidi da libro "space prop. analysis and design" (Humble)
     disp('OXIDIZER LOX')
-    Vs = 3.0 ; % [rad/s]? -> stage specific speed per altre specie;
+    Ns = 3.0 ; % [ - ] è adimensionale -> stage specific speed per altre specie;
     u_ss = 90; % suction specific speed for cryogenic liquids
     psi = 0.55 ; % pump head coefficient for others 
 end
 
 H = Dp/(g*rho); % head of the pump
-Ns = ceil(Dp/Dp_ps) ; % numero di stadi necessari alla pompa -> se <1 Ns = 1 effettuato da funzione ceil
+n = ceil(Dp/Dp_ps) ; % numero di stadi necessari alla pompa -> se <1 Ns = 1 effettuato da funzione ceil
+
+
 
 Q = mdot/rho; % [m^3/s] -> portata volumetrica
 NPSH = (P_in - P_vapor)/(g*rho); % [m] Net Positive Suction Head-> misura per evitare cavitazione
-% Vr = (u_ss*(NPSH^0.75))/sqrt(Q); % da usare se pompa preceduta da booster
+Vr_npsh = (u_ss*(NPSH^0.75))/sqrt(Q); % da usare se pompa preceduta da booster
 % -> per le seconde pompe è da usare questo? considero le prime dei
 % booster?
-Vr = (Vs*(H/Ns)^0.75)/sqrt(Q); % [rad/s]
+Vr = (Ns*(H/n)^0.75)/sqrt(Q); % [rad/s]
+% Se invece non è preceduto da booster va usato il valore minore
+if Vr > Vr_npsh
+    disp('Uso valore alternativo di Vr');
+    Vr = Vr_npsh;
+end
+%Ns = 5018 * sqrt(Q)/(H/n)^(.75);
+
+
 V = 30*Vr/pi; % [rpm] -> trasformo da [rad/s] in [rpm] -> da verificare formula
 % Vs = Vr*sqrt(Q)/((H/Ns)^0.75) ;  % [rad/s]? -> stage specific speed rivalutata -> serve a valutare efficienza -> usa grafico
 beta = P_out/P_in ; %rapporto di compressione della pompa
 
 % pump impeller speed
-u_t = sqrt((g*H)/(Ns * psi)) ; %a cosa serve?
+u_t = sqrt((g*H)/(n * psi)) ; %a cosa serve?
 
 % pump impeller diameters
 phi = 0.10 ;% inducer-inlet flow coefficient
@@ -45,7 +55,7 @@ D1t = (((4/pi)*Q)/(phi*Vr)/(1-L^2))^(1/3);% [m] inlet tip diameter
 
 P_req = (g*mdot*H*10^(-6))/eff; %power required from the turbine to drive the pump
 Dh = P_req/mdot; % salto entalpico
-Dt = Dh/cp;%variazione di temperatura-> molto meno importante dell'aumento della pressione
+Dt = Dh/cp; %variazione di temperatura-> molto meno importante dell'aumento della pressione
 %nei due passaggi precedenti c'è qualche errore
 T_out = Dt + T_in ;
 %T_out troppo bassa
@@ -56,7 +66,7 @@ tp_design.head_m = H;
 tp_design.temp_out_K= T_out ;
 tp_design.rapp_compressione = beta ;
 tp_design.NPSH = NPSH ;
-tp_design.num_stadi = Ns ;
+tp_design.num_stadi = n ;
 tp_design.vel_rotazione_radpersecond = Vr ;
 tp_design.vel_rot_rpm = V ; 
 tp_design.salto_entalpico = Dh ;
