77564efdfd855a6de332e8dae504ede173c05ae9
Utisse <lorenzopasqui@gmail.com>
2024-08-02 23:02:47 +0200

    Il valore del pressurizzante converge

--- tank_design_script.m
+++ tank_design_script.m
@@ -1,4 +1,4 @@
-%%
+ %%
 clear;clc;close all;
 
     g0 = 9.81;
@@ -7,12 +7,13 @@ clear;clc;close all;
     Thrust_sl = 1860e3; %[N]
     % burn time di tutti e tre i motori 
     burn_time = 480;
+    I_tot =  Thrust_sl * burn_time; %[N s]
+    I_sp = 366; %[s] sea level
     % moltiplico per 3 poichè ho tre RS-25 e o il burntime è triplicato
     % oppure lo è la spinta
-    I_tot = 3* Thrust_sl * burn_time; %[N s]
-    I_sp = 366; %[s] sea level
-    m_prop = I_tot/(I_sp * g0);
+    m_prop = 3 * I_tot/(I_sp * g0);
     of = 6.03;
+
     LH.name = "Idrogeno";
     LH.m = m_prop/(1 + of);
     LH.temp = 20.3;
@@ -24,21 +25,25 @@ clear;clc;close all;
     LH.cp = 9673.283384191343;
     LH.cv = 5657.830299878916;
     LH.gamma = LH.cp/LH.cv;
+    LH.gammaPress = 1.5102; 
     LH.massa_molare =  1.00784*2;
 
 
     ox.name = "Ossigeno";
     ox.m = LH.m * of;
-    ox.rho = 1141.4844624986936;
+    ox.rho = 1142.4497675514076; %[kg/m3]
     ox.temp = 90.19;
     ox.Tpress = 448.7056;
     ox.Ppress = 2.4607e+07;
-    ox.pressure = 246e3;
+    ox.gammaPress = 1.5102; 
+    ox.pressure = .68e6;
     ox.volume = ox.m/ox.rho;
     ox.cp = 1698.1015309831562;
     ox.cv = 929.8734290792967;
     ox.gamma = ox.cp/ox.cv;
-    ox.massa_molare = 32;
+    ox.massa_molare = 32; %[kg/kmol]
+
+
     % In realtà questi due serbatoi sono più grandi poichè 
     % non tutto il propellente viene usato a fini propulsivi (i.e. va in
     % camera di combustione).
@@ -67,57 +72,57 @@ clear;clc;close all;
     % uso il metodo pV/W 
     % phi_tank = 2500; % [m], tipicamente per serbatoi metallici
     % g_0 = 9.81;
-    R = 8314;
+    R = 8314; %[m^3 Pa /(K kmol)]
     %%
     % Calcolo pressurizzante, questo passaggio in teoria mi avvicinerà ai
     % valori reali.
     for i = 1:length(prop) % Eseguo il calcolo per ogni propellente
     p = prop(i);
     disp(" --- " + p.name + " ---");
-    
-    press_volume = 0;
-    press_volume_old = 0;
-    iter = 1;
+    maxiter = 1000;
+    press_volume = zeros(maxiter,1);
+    iter = 2;
     Ru = R / p.massa_molare;
     
     % Considero un lungo burn time e dunque una trasformazione isoentropica
-    T_f = p.Tpress * (p.pressure / p.Ppress)^((p.gamma - 1) / p.gamma)
-    
-        while 1
-            iter = iter + 1;
-            
-            % Aggiustiamo il volume totale del serbatoio
-            p.volume = press_volume + p.volume;
-            
+    T_f = p.Tpress * (p.pressure / p.Ppress)^((p.gamma - 1) / p.gamma);
+        while 1            
+            press_volume(iter) = press_volume(iter-1) + p.volume;
+
             % Calcoliamo la massa del gas pressurizzante
-            press_mass = p.volume * p.pressure / (Ru * T_f);
+            press_mass = press_volume(iter) * p.pressure / (Ru * T_f);
             
             % Calcoliamo il nuovo volume del gas pressurizzante
-            new_press_volume = press_mass * Ru * p.temp / p.pressure;
-
+            press_volume(iter) = press_mass /p.rho;
             % Verifichiamo la condizione di convergenza
-            if abs(new_press_volume - press_volume_old) < 1e-5 || iter > 1000
+            check = abs(press_volume(iter) - press_volume(iter-1));
+            if check < 1e-10|| iter > maxiter
                 disp("Convergiamo a iterazione = " + iter);
-                disp("Volume pressurizzante = " + new_press_volume);
+                disp("Volume pressurizzante = " + press_volume(iter));
+                disp("Temperatura finale = " + T_f)
+                disp("Differenza finale = "+ check)
                 break;
             end
         
         % Aggiorniamo il volume e la vecchia iterazione
-            press_volume_old = press_volume;
-            press_volume = new_press_volume;
+            iter = iter + 1;
         end
+       
+
     end
-    %% calotte sferiche 
-    V_sfera = 4/3 * pi* r^3;
-    A_sfera = 4*pi * r^2;
-    spessore_sfera = p_b *r /(2*F_all);
-    m_sfera = A_sfera *spessore_sfera *rho; 
-    %% Cilindro 
-    % lunghezza della sezione cilindrica
-    V_cilindro = pi*r* l_c;
-    A_cilindro = 2 * pi * r * l_c;
-    spessore_cilindro = p_b * r /(F_all);
-    m_cilindro = A_cilindro * spessore_cilindro * rho;
-    V_press = abs(V(end)- V_tot);
-    tank = Tank(V(end),V_press,T_f,P_f);
+
     
+    %% calotte sferiche 
+%     V_sfera = 4/3 * pi* r^3;
+%     A_sfera = 4*pi * r^2;
+%     spessore_sfera = p_b *r /(2*F_all);
+%     m_sfera = A_sfera *spessore_sfera *rho; 
+%     %% Cilindro 
+    % lunghezza della sezione cilindrica
+%     V_cilindro = pi*r* l_c;
+%     A_cilindro = 2 * pi * r * l_c;
+%     spessore_cilindro = p_b * r /(F_all);
+%     m_cilindro = A_cilindro * spessore_cilindro * rho;
+%     V_press = abs(V(end)- V_tot);
+%     tank = Tank(V(end),V_press,T_f,P_f);
+%     
